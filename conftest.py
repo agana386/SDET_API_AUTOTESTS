c8e97cf5ab44fb04e5cc48f858e77d80c489fc81
agana386 <olga-pasynkova@yandex.ru>
2026-04-23 03:11:05 +0700

    clear main

--- conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from clients.api_client import ApiClient
-
-
-@pytest.fixture
-def client():
-    return ApiClient()
--- models/entity.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class Post(BaseModel):
-    id: int | None = None
-    title: str
-    body: str
-    userId: int
